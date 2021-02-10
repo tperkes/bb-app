e27a025a6a7019a308b9f08b2e77ff0447121083
Tyler <tyance2@gmail.com>
2021-02-10 11:31:12 -0700

    130 complete

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => { // see 129 starting at 1:45. we are converting the state object passed from BurgerBuilder.js into an array of ingredients
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => { // see 129 starting at 1:45. we are converting the state object passed from BurgerBuilder.js into an array of ingredients
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    }); 
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
