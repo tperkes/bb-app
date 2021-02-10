ab28a45d21d2b06b6ba60530fc212f5bf1fec152
Tyler <tyance2@gmail.com>
2021-02-10 09:04:45 -0700

    125 complete

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxilary from '../../hoc/Auxilary';
+import Classes from '../Layout/Layout.css';
 
 const layout = ( props ) => (
     <Auxilary>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={Classes.Content}> 
         {props.children}
     </main>
     </Auxilary>
