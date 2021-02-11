b4c1dc5b15ea7acdac870d9fb508b13952b48225
Tyler <tyance2@gmail.com>
2021-02-11 11:00:14 -0700

    137 complete

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+
 import Burger from '../../components/Burger/Burger'
 import Aux from '../../hoc/Auxilary';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.25,
@@ -81,6 +84,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
