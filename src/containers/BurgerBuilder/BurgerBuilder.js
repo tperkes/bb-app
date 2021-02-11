a9be22ac5cdd1e7f43a67a0dfa858bc21df1482f
Tyler <tyance2@gmail.com>
2021-02-11 10:05:35 -0700

    135 complete

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,7 +71,9 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}/>
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}/>
+
             </Aux>
         );
     }
